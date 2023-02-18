3578946ef9269f66202a62032d1d09228970b11d
Mayesha Hridula <mayesha.fairuz.work@gmail.com>
2023-02-18 23:22:29 +0600

    added more functionality on promise examples

--- promises.js
+++ promises.js
@@ -6,3 +6,12 @@ const futureData = fetch('https://twitter.com/will/tweets/1')
 futureData.then(display); // input from the twitter API link
 
 console.log('Me first!');
+
+function display(data){console.log(data)}
+function printHello(){console.log("Hello");}
+function blockFor300ms(){/* blocks js thread for 300ms */}
+setTimeout(printHello, 0);
+const futureData2 = fetch('https://twitter.com/will/tweets/1')
+futureData2.then(display)
+blockFor300ms()
+console.log("Me first!");
